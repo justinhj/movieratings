0b89ec3b2c256c5ad20dff32cc27ae9829a942bd
Justin Heyes-Jones <justinhj@gmail.com>
2014-01-01 21:26:16 -0800

    Update to latest versions of Clojure and utilised libraries. Enable handling of compressed response from RT

--- project.clj
+++ project.clj
@@ -1,13 +1,19 @@
-(defproject rottentomatoes "1.0.2-SNAPSHOT"
+(defproject rottentomatoes "1.0.3-SNAPSHOT"
   :description "Clojure code to grab movie ratings from Rotten Tomatoes using their API or screen scraping if no api key is found"
+  :url "http://example.com/FIXME"
   :dependencies [
-		 [org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-		 [http.async.client "0.2.1"]
-                 [cheshire "4.0.1"]
+		 [org.clojure/clojure "1.5.1"]
+		 [http.async.client "0.5.2"]
+                 [cheshire "5.3.0"]
 		 ]
-  :main rottentomatoes.core
-  :dev-dependencies [
-		     [swank-clojure "1.2.1"]
-		      ]
-  )
+  :main rottentomatoes.core)
+
+;; (defproject rottentomatoes/rottentomatoes "1.0.2-SNAPSHOT" 
+;;   :dependencies [[org.clojure/clojure "1.2.0"]
+;;                  [org.clojure/clojure-contrib "1.2.0"]
+;;                  [http.async.client "0.2.1"]
+;;                  [cheshire "4.0.1"]]
+;;   :main rottentomatoes.core
+;;   :min-lein-version "2.0.0"
+;;   :plugins [[swank-clojure "1.4.2"]]
+;;   :description "Clojure code to grab movie ratings from Rotten Tomatoes using their API or screen scraping if no api key is found")
--- src/rottentomatoes/core.clj
+++ src/rottentomatoes/core.clj
@@ -2,26 +2,30 @@
   (:gen-class)
   (:require
    [clojure.string :as cs]
-   [clojure.contrib.str-utils2 :as s] 
    [http.async.client :as c]
    [cheshire.core :as json]))
 
 (import [java.net URLEncoder])
 
-(def *base-url* "http://www.rottentomatoes.com")
+(def base-url "http://www.rottentomatoes.com")
 
-(def *search-end-point* "/search/?search=")
+(def search-end-point "/search/?search=")
 
 ;; Rotten Tomatoes API key
 ;; Need to set it as an environment variable in your system
 
-(def *API-KEY* (System/getenv "ROTTEN_TOMATOES_API_KEY"))
+(def ^:dynamic *api-key* (System/getenv "ROTTEN_TOMATOES_API_KEY"))
+
+;; note if you're not able to set an environment variable can fix at runtime for example like this:
+;; (binding [*api-key* "YOUR KEY HERE"] (-main "jaws"))
+
+(def http-client (c/create-client :compression-enabled true))
 
 ;; Example http://www.rottentomatoes.com/search/?search=jaws&sitesearch=rt
 
 (defn first-match-after [re1 re2 seq]
   "Splits the sequence SEQ using RE1 then searches after the first match and before the next match for the first occurence of RE2"
-  (let [[_ _ after] (s/partition seq re1)]
+  (let [[_ _ after] (cs/split seq re1)] ;;; note changed s/partition to split
     (re-find re2 after))) 
 
 (defn response-status-code [resp]
@@ -29,14 +33,27 @@
 
 (defn scoop-url [url]
   "Use the http client to do a GET on the url"
-;  (prn "getting " url) ; debug print
-  (let [resp (c/GET url)]
-    (c/await resp)
-    (if (= 301 (response-status-code resp)) ; redirect
+  (println "getting " url) ; debug print
+  (let [resp (c/GET http-client url)
+        code (:code (c/status resp))
+;;        headers (c/headers resp)
+;;        content-type (c/content-type resp)
+        body (-> resp
+        c/await c/string)]
+;;    (println "code " code)
+;;    (println "headers " headers)
+;;    (println "content-type " content-type)
+    (if (not= code 200)
       (do
-       (prn "page is redirected")
-       [(response-status-code resp) nil])
-      [(response-status-code resp) (c/string resp)])))
+;;        (println "Response returned code " code)
+        [(response-status-code resp) nil])
+      (if (nil? body)
+        (do
+;;          (println "Response returned empty body")
+          [(response-status-code resp) nil])
+        (do
+;;          (println "Response" body) ;;  (c/string body))
+          [(response-status-code resp) body])))))
 
 ;; Get movie urls
 ;; Does a search of Rotten Tomatoes for the search text, then scrapes the results
@@ -45,18 +62,18 @@
 
 (defn get-movie-urls [search-text]
   (let [encoded-search-text (URLEncoder/encode search-text)
-	[code body] (scoop-url (str *base-url* *search-end-point* encoded-search-text))
+	[code body] (scoop-url (str base-url search-end-point encoded-search-text))
 	anchor-text #"<h3 class=\"bottom_divider\">Movies</h3>"
 	]
     (if (= code 200)
-      (let [[_ _ after] (s/partition body anchor-text)]
+      (let [[_ _ after] (cs/split body anchor-text)]
 	(if (nil? after)
 	  (do
 	   (print "ERROR: failed to find required text \"" anchor-text "\"")
 	   nil)
-	  (let [[_ & results] (s/partition after #"\"(/m/.*/)\"")]
-	    (map #(str *base-url* (second %)) (take-nth 2 results)))))
-      (prn "IO error " code))))
+	  (let [[_ & results] (cs/split after #"\"(/m/.*/)\"")]
+	    (map #(str base-url (second %)) (take-nth 2 results)))))
+      (println "IO error " code))))
 
 ;; Given a movie url GET the page then scrape it for the citic and audience rating
 
@@ -91,12 +108,12 @@
 ;; The API makes us find the movies query URL before we can start
 
 (defn get-rt-api-url []
-  (let [[code body] (scoop-url (str "http://api.rottentomatoes.com/api/public/v1.0.json?apikey=" *API-KEY*))]
+  (let [[code body] (scoop-url (str "http://api.rottentomatoes.com/api/public/v1.0.json?apikey=" *api-key*))]
     (when (= code 200) 
       (:movies (:links (json/parse-string body true))))))
 
 (defn get-rt-api-template [movies-url]
-  (let [[code body] (scoop-url (str movies-url "?apikey=" *API-KEY*))]
+  (let [[code body] (scoop-url (str movies-url "?apikey=" *api-key*))]
     (when (= code 200) 
       (:link_template (json/parse-string body true)))))
 
@@ -104,7 +121,7 @@
   (let [str0 (cs/replace template "{search-term}" (URLEncoder/encode search-term))
         str1 (cs/replace str0 "{results-per-page}" "50")
         str2 (cs/replace str1 "{page-number}" "1")
-        fin (str str2 "&apikey=" *API-KEY*)]
+        fin (str str2 "&apikey=" *api-key*)]
     fin))
 
 (defn get-rt-movie-data [movie-url]
@@ -127,16 +144,18 @@
 (defn -main [& args]
   (if (= (count args) 1)
     (do
-      (if *API-KEY*
+      (println "Query Rotten Tomatoes API with key " *api-key*)
+      (if *api-key*
         (let [movies-url (get-rt-api-url)
               template (get-rt-api-template movies-url)
               movie-search-url (get-rt-api-movies template (first args))
               movie-data (get-rt-movie-data movie-search-url)]
           (print-rt-movie-data movie-data))
         (do
-          (pmap-get-movie-ratings (first args))
-          (println "Done"))))
+          (println "Web scraping method disabled due to html format change")
+          (println "Please obtain an API key and set it as the environmment variable ROTTEN_TOMATOES_API_KEY")
+;;          (println "Using web scraping method")
+;;          (pmap-get-movie-ratings (first args))
+;;          (println "Done")))
+          )))
     (println "Enter a search string to match in movie titles")))
-
-
-
